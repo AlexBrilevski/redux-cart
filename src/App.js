002383faf1c0d965a3190ac1bdeefaa03314fd1d
Alex Brilevski <brilevski.alex@gmail.com>
2025-12-17 20:54:49 +0300

    Storing cart data in Firebase database on app load and each cart update was added

--- src/App.js
+++ src/App.js
@@ -1,10 +1,18 @@
 import { useSelector } from 'react-redux';
+import { useEffect } from 'react';
 import Cart from './components/Cart/Cart';
 import Layout from './components/Layout/Layout';
 import Products from './components/Shop/Products';
 
 function App() {
+  const cart = useSelector(state => state.cart);
   const isCartOpen = useSelector(state => state.ui.isCartOpen);
+
+  useEffect(() => {
+    fetch('https://react-products-database-e5f44-default-rtdb.europe-west1.firebasedatabase.app/cart.json', 
+      {method: 'PUT', body: JSON.stringify(cart)}
+    );
+  }, [cart]);
 
   return (
     <Layout>
